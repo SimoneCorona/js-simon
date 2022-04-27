05f1f08ca1f59b0551208aa3d9dc39a5fcc23e4d
SimoneCorona <corona.simone93@gmail.com>
2022-04-27 16:34:27 +0200

    not working

--- js/script.js
+++ js/script.js
@@ -12,31 +12,32 @@ for (let i = 0; i < 5 ; i++) {
 //Stampa dei 5 num nel HTML
 document.getElementById("numberToRemember").innerHTML = fiveNum;
 
-setTimeout(timerGame, 1000);
+setTimeout(timerGame, 3000);
 
 function timerGame() {
     //Nascondere i 5 num casuali
-    let fiveNum = document.getElementById("numberToRemember").classList;
     fiveNum.add("hidden");
-    
-    //Array dei 5 num scelti dall' utente
-    let userFiveNum = [];
-    for (let i = 0; i < 5; i++) {
-        const userNum = parseInt(prompt("Inserisci un numero"));
-        userFiveNum.push(userNum);
-    }
-    //Confronto array num random con l' array num scelti dall' utente
-    let userPoints = [];
-    for (let i = 0; i < 5; i++) {
-        if (fiveNum[i] === userFiveNum[i]) {
-            userPoints.push;
-        } 
-    }
-
-    //Stampa risultato
-    fiveNum.remove("hidden");
-    document.getElementById("userNumberChoice").innerHTML = userFiveNum;
-    document.getElementById("score").innerHTML = `Hai azzeccato ${userPoints.length}`;
 };
-    
+
+setStartGame(timerGame, 3100);
+
+function startGame() {
+//Array dei 5 num scelti dall' utente
+let userFiveNum = [];
+for (let i = 0; i < 5; i++) {
+    const userNum = parseInt(prompt("Inserisci un numero"));
+    userFiveNum.push(userNum);
+}
+//Confronto array num random con l' array num scelti dall' utente
+let userPoints = [];
+for (let i = 0; i < 5; i++) {
+    if (fiveNum[i] === userFiveNum[i]) {
+        userPoints.push;
+    } 
+}
+//Stampa risultato
+fiveNum.remove("hidden");
+document.getElementById("userNumberChoice").innerHTML = userFiveNum;
+document.getElementById("score").innerHTML = `Hai azzeccato ${userPoints.length} numeri, quelli uguali erano ${userFiveNum}`;
+}; 
 
